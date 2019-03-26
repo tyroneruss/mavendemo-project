3228893ddb62b4b4b813f3f4872c4b15afac8ace
TyroneRuss <tyroneruss.aws#gmail.com>
2019-03-25 22:11:20 -0400

    Changed java file

--- mavendemo/src/main/java/com/learnmaven/mavendemo/App.java
+++ mavendemo/src/main/java/com/learnmaven/mavendemo/App.java
@@ -10,9 +10,9 @@ public class App
 	public static void main( String[] args )
     {
 		Calculator calc = new Calculator();
-		calc.add(1, 2);
+		calc.add(1, 5);
 		calc.subtract(4, 2);
-		calc.multiply(10, 10);
+		calc.multiply(10, 15);
 		calc.divide(100, 10);		
     }
 }
